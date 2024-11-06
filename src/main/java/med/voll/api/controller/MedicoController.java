6773c73eb58b28ae27834a6935ff3ecf1d69d9eb
johanssonlucena <johansson.santos@ccc.ufcg.edu.br>
2024-11-06 14:08:33 -0300

    Entidades e Repository

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,20 +1,25 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import med.voll.api.medicos.CadastroMedicos;
+import med.voll.api.medicos.Medico;
+import med.voll.api.medicos.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+	
+	@Autowired
+	private MedicoRepository repository;
 
 	@PostMapping
 	public void cadastrar(@RequestBody CadastroMedicos dados) {
-		System.out.println(dados);
-		
+		repository.save(new Medico(dados));
 	}
 	
 }
